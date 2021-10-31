ba2c1e8f17f6109264772352642e8c1c9128b6ac
Vinoth Pandian <vinothpandian@live.com>
2021-10-31 17:55:12 +0100

    add CORS

--- comments/app.py
+++ comments/app.py
@@ -1,13 +1,20 @@
-
 from typing import Dict
 from uuid import uuid4
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 from pydantic import BaseModel
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
 
 class Comment(BaseModel):
     post_id: str
--- query/app.py
+++ query/app.py
@@ -1,12 +1,19 @@
 from typing import List
-from uuid import uuid4
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 from pydantic import BaseModel
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
 
 class Comment(BaseModel):
     id: str
