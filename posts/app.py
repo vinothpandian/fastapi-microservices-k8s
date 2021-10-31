c7826503c33d3409e8746fb6eb020a7bb9fc68e6
Vinoth Pandian <vinothpandian@live.com>
2021-10-31 16:25:14 +0100

    add posts service

---
+++ posts/app.py
@@ -0,0 +1,30 @@
+from logging import debug
+from typing import List
+from uuid import uuid4
+
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class CreatePostRequest(BaseModel):
+    title: str
+
+class Post(CreatePostRequest):
+    id: str
+
+posts: List[Post] = []
+
+
+@app.post("/posts/create")
+def create_post(create_post_request: CreatePostRequest):
+
+    id: str = uuid4().hex
+    post = Post(id=id, title=create_post_request.title)
+    posts.append(post)
+    return post
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, debug=True, host="0.0.0.0", port=4000)
