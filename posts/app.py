75481d90e18025793384477fc1bee71f1e7a4343
Vinoth Pandian <vinothpandian@live.com>
2021-10-31 16:30:34 +0100

    add logging to posts

--- posts/app.py
+++ posts/app.py
@@ -1,9 +1,9 @@
-from logging import debug
 from typing import List
 from uuid import uuid4
 
 import uvicorn
 from fastapi import FastAPI
+from loguru import logger
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -23,6 +23,7 @@ def create_post(create_post_request: CreatePostRequest):
     id: str = uuid4().hex
     post = Post(id=id, title=create_post_request.title)
     posts.append(post)
+    logger.debug(f"Post created with id: {id}")
     return post
 
 
